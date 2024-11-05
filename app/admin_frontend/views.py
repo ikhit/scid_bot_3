222ba9e0df87f11626438370ba89cace2e11ed8e
ikhit <furyheart@mail.ru>
2024-11-05 22:13:31 +0300

    trying add css styles

--- app/admin_frontend/views.py
+++ app/admin_frontend/views.py
@@ -1,11 +1,17 @@
+from quart import render_template
 
 from . import app
 from crud import products_crud
 from core.db import AsyncSessionLocal
 
 
-@app.route("/", methods = ["GET"])
+@app.route("/", methods=["GET"])
 async def index():
+    return await render_template("base.html")
+
+
+@app.route("/porducts")
+async def products():
     async with AsyncSessionLocal() as session:
-        data = await products_crud.get(1, session)
-    return data.name
+        products = await products_crud.get_multi(session)
+    return await render_template("products.html", products=products)
