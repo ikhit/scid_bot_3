cd3b6a49327df645e07962a2f2e83e53dcff0ecc
ikhit <furyheart@mail.ru>
2024-11-06 22:14:37 +0300

    add buttons to frontend, fix styles, add endpoint for product details

--- app/admin_frontend/__init__.py
+++ app/admin_frontend/__init__.py
@@ -1,7 +1,7 @@
 from quart import Quart
 from .config import Config
 
-app = Quart(__name__, static_folder='app/admin_frontend/static', static_url_path='/static')
+app = Quart(__name__, static_folder='static', static_url_path='/static')
 app.config.from_object(Config)
 
 from . import views  # noqa
