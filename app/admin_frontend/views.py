17f828273221e06d012bb3a20ae2e8ad69b50559
ikhit <furyheart@mail.ru>
2024-11-09 10:31:27 +0300

    frontend: add endpoints for the rest bot user info models

--- app/admin_frontend/views.py
+++ app/admin_frontend/views.py
@@ -2,7 +2,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from quart import render_template
 
 from . import app
-from crud import products_crud, category_product_crud, portfolio_crud
+from crud import (
+    products_crud,
+    category_product_crud,
+    portfolio_crud,
+    info_crud,
+    company_info_crud,
+)
+from models.models import QuestionEnum
 from .utils import get_file_url, db_session
 
 
@@ -16,7 +23,11 @@ async def index():
 async def get_products(session: AsyncSession):
     """Вывести список всех продуктов."""
     products = await products_crud.get_multi(session)
-    return await render_template("products.html", products=products)
+    return await render_template(
+        "list.html",
+        data_list=products,
+        details_url="get_product_details",
+    )
 
 
 @app.route("/products/<int:id>", methods=["GET"])
@@ -31,7 +42,7 @@ async def get_product_details(session: AsyncSession, id: int):
         if data.media:
             data.media = get_file_url(data.media)
     return await render_template(
-        "prdouct_detail.html", product=product, product_data=product_data
+        "detail.html", item=product, item_data=product_data
     )
 
 
@@ -39,11 +50,66 @@ async def get_product_details(session: AsyncSession, id: int):
 @db_session
 async def get_projects(session: AsyncSession):
     projects = await portfolio_crud.get_multi(session)
-    return await render_template("projects.html", projects=projects)
+    return await render_template(
+        "list.html",
+        data_list=projects,
+        details_url="get_project_details",
+    )
 
 
 @app.route("/projects/<int:id>")
 @db_session
 async def get_project_details(session: AsyncSession, id: int):
     project = await portfolio_crud.get(id, session)
-    return await render_template("project_detail.html", project=project)
+    return await render_template("detail.html", item=project)
+
+
+@app.route("/questions")
+@db_session
+async def get_questions(session: AsyncSession):
+    questions = await info_crud.get_all_questions_by_type(
+        QuestionEnum.GENERAL_QUESTIONS, session
+    )
+    return await render_template(
+        "list.html", data_list=questions, details_url="get_question_details"
+    )
+
+
+@app.route("/questions/<int:id>")
+@db_session
+async def get_question_details(session: AsyncSession, id: int):
+    question = await info_crud.get(id, session)
+    return await render_template("detail.html", item=question)
+
+
+@app.route("/problems")
+@db_session
+async def get_product_problems(session: AsyncSession):
+    questions = await info_crud.get_all_questions_by_type(
+        QuestionEnum.PROBLEMS_WITH_PRODUCTS, session
+    )
+    return await render_template(
+        "list.html", data_list=questions, details_url="get_product_problems_details"
+    )
+
+
+@app.route("/problems/<int:id>")
+@db_session
+async def get_product_problems_details(session: AsyncSession, id: int):
+    question = await info_crud.get(id, session)
+    return await render_template("detail.html", item=question)
+
+@app.route("/about-company")
+@db_session
+async def get_company_about(session: AsyncSession):
+    infos = await company_info_crud.get_multi(session)
+    return await render_template(
+        "list.html", data_list=infos, details_url="get_company_about_details"
+    )
+
+
+@app.route("/about-company/<int:id>")
+@db_session
+async def get_company_about_details(session: AsyncSession, id: int):
+    info = await company_info_crud.get(id, session)
+    return await render_template("detail.html", item=info)
