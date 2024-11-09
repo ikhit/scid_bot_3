dd05894bf529fe231e2c61d65e27e53e77fd5912
ikhit <furyheart@mail.ru>
2024-11-09 07:28:36 +0300

    add projects tree

--- app/admin_frontend/views.py
+++ app/admin_frontend/views.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from quart import render_template
 
 from . import app
-from crud import products_crud, category_product_crud
+from crud import products_crud, category_product_crud, portfolio_crud
 from .utils import get_file_url, db_session
 
 
@@ -19,14 +19,14 @@ async def get_products(session: AsyncSession):
     return await render_template("products.html", products=products)
 
 
-@app.route("/products/<int:product_id>", methods=["GET"])
+@app.route("/products/<int:id>", methods=["GET"])
 @db_session
-async def get_product_details(session: AsyncSession, product_id: int):
+async def get_product_details(session: AsyncSession, id: int):
     """Вывести всю информацию о продукте."""
     product_data = await category_product_crud.get_category_by_product_id(
-        product_id, session
+        id, session
     )
-    product = await products_crud.get(product_id, session)
+    product = await products_crud.get(id, session)
     for data in product_data:
         if data.media:
             data.media = get_file_url(data.media)
@@ -36,4 +36,14 @@ async def get_product_details(session: AsyncSession, product_id: int):
 
 
 @app.route("/projects")
-async def projects(): ...
+@db_session
+async def get_projects(session: AsyncSession):
+    projects = await portfolio_crud.get_multi(session)
+    return await render_template("projects.html", projects=projects)
+
+
+@app.route("/projects/<int:id>")
+@db_session
+async def get_project_details(session: AsyncSession, id: int):
+    project = await portfolio_crud.get(id, session)
+    return await render_template("project_detail.html", project=project)
